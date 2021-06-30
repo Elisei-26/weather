77b1ac11b6d7c30d79f848c66872dc988c278c8d
Elisei-26 <81551154+Elisei-26@users.noreply.github.com>
2021-06-30 13:37:21 +0100

    minor changes

--- src/components/WeatherStatus.jsx
+++ src/components/WeatherStatus.jsx
@@ -6,72 +6,64 @@ function WeatherStatus(props) {
     <Grid container spacing={3} style={ { width: 1320 } }>
       <Grid item xs={6}>
         <Paper elevation={8} style={ { width: 500 } }>
-          <center>
-            <font size="5">
-              <h3>
-                City name: {props.weatherInfo.location?.name}
-              </h3>
-              <h3>
-                Local time: {props.weatherInfo.location?.localtime}
-              </h3>
-              <h3>
-                Last update: {props.weatherInfo.current?.last_updated}
-              </h3>
-              <h3>
-                Temperature in °C: {props.weatherInfo.current?.temp_c} 
-              </h3>
-              <h3>
-                Temperature in °F: {props.weatherInfo.current?.temp_f} 
-              </h3>
-              <h3>
-                Condition: {props.weatherInfo.current?.condition.text}
-              </h3>
-              <h3>
-                <img src={props.weatherInfo.current?.condition.icon} alt="img" />
-              </h3>
-              <h3>
-                Wind speed in kph: {props.weatherInfo.current?.wind_kph}
-              </h3>
-              <h3>
-                Wind speed in mph: {props.weatherInfo.current?.wind_mph}
-              </h3>
-            </font>
-          </center>
+          <h4 align="left">
+            City name: {props.weatherInfo.location?.name}
+          </h4>
+          <h4 align="left">
+            Local time: {props.weatherInfo.location?.localtime}
+          </h4>
+          <h4 align="left">
+            Last update: {props.weatherInfo.current?.last_updated}
+          </h4>
+          <h4 align="left">
+            Temperature in °C: {props.weatherInfo.current?.temp_c} 
+          </h4>
+          <h4 align="left">
+            Temperature in °F: {props.weatherInfo.current?.temp_f} 
+          </h4>
+          <h4 align="left">
+            Condition: {props.weatherInfo.current?.condition.text}
+          </h4>
+          <h4 align="left">
+            <img src={props.weatherInfo.current?.condition.icon} alt="img" />
+          </h4>
+          <h4 align="left">
+            Wind speed in kph: {props.weatherInfo.current?.wind_kph}
+          </h4>
+          <h4 align="left">
+            Wind speed in mph: {props.weatherInfo.current?.wind_mph}
+          </h4>
         </Paper>
       </Grid>
       <Grid item xs={6}>
         <Paper elevation={8} style={ { width: 500 } }>
-          <center>
-            <font size="5">
-              <h3 style={ { alignContent: 'center' } }>
-                Wind degree: {props.weatherInfo.current?.wind_degree} 
-              </h3>
-              <h3>
-                Pressure mb: {props.weatherInfo.current?.pressure_mb} 
-              </h3>
-              <h3>
-                Pressure in: {props.weatherInfo.current?.pressure_in} 
-              </h3>
-              <h3>
-                Precipitations mm: {props.weatherInfo.current?.precip_mm}
-              </h3>
-              <h3>
-                Precipitations in: {props.weatherInfo.current?.precip_in}
-              </h3>
-              <h3> 
-                Humidity: {props.weatherInfo.current?.humidity} 
-              </h3>
-              <h3>
-                Cloud: {props.weatherInfo.current?.cloud} 
-              </h3>
-              <h3>
-                Feelslike °C: {props.weatherInfo.current?.feelslike_c} 
-              </h3>
-              <h3>
-                Feelslike °F: {props.weatherInfo.current?.feelslike_f} 
-              </h3>
-            </font>
-          </center>
+          <h4 align="left">
+            Wind degree: {props.weatherInfo.current?.wind_degree} 
+          </h4>
+          <h4 align="left">
+            Pressure mb: {props.weatherInfo.current?.pressure_mb} 
+          </h4>
+          <h4 align="left">
+            Pressure in: {props.weatherInfo.current?.pressure_in} 
+          </h4>
+          <h4 align="left">
+            Precipitations mm: {props.weatherInfo.current?.precip_mm}
+          </h4>
+          <h4 align="left">
+            Precipitations in: {props.weatherInfo.current?.precip_in}
+          </h4>
+          <h4 align="left"> 
+            Humidity: {props.weatherInfo.current?.humidity} 
+          </h4>
+          <h4 align="left">
+            Cloud: {props.weatherInfo.current?.cloud} 
+          </h4>
+          <h4 align="left">
+            Feelslike °C: {props.weatherInfo.current?.feelslike_c} 
+          </h4>
+          <h4 align="left">
+            Feelslike °F: {props.weatherInfo.current?.feelslike_f} 
+          </h4>
         </Paper>
       </Grid>
     </Grid>
